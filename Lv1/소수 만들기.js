d1a103128420d79fdfc7636ce2bcceacbe8d7d73
songbbang <rokaf340@gmail.com>
2024-01-15 16:18:43 +0900

    조합 문제

--- Lv1/소수 만들기.js
+++ Lv1/소수 만들기.js
@@ -1,16 +1,47 @@
+const getCombinations = (arr, num) => {
+  const results = [];
+
+  // nC1 이며, 1이면 의미 없기때문에 바로 반환한다.
+  if (num === 1) return arr.map((v) => [v]);
+  arr.forEach((fixed, index, origin) => {
+    // console.log(arr, fixed);
+
+    // 조합에서는 값 순서에 상관없이 중복이 되면 안되기 때문에 현재값 이후의 배열들만 추출한다.
+    const rest = origin.slice(index + 1);
+    // console.log('fixed =', fixed, 'rest =', rest);
+    // console.log('num =', num);
+    // 나머지 배열을 기준으로 다시 조합을 실시한다.
+    // 기준값(fixed)이 있기 때문에 선택하려는 개수에서 - 1 을 해준다.
+    const combinations = getCombinations(rest, num - 1);
+    // console.log('combinations =', combinations);
+    // 기준값(fixed)에 돌아온 조합(combinations)을 붙인다.
+    const attached = combinations.map((v) => [fixed, ...v]);
+    // console.log('attached =', attached);
+
+    // 붙인 값을 결과 값에 넣어준다.
+    results.push(...attached);
+    // console.log('results =', results);
+    // console.log('');
+  });
+
+  return results;
+};
+
 function solution(nums) {
-  let answer = -1;
-  let arr = [];
-  for (let i = 0; i < nums.length; i++) {
-    let arr2 = [];
-    arr2.push(nums[i]);
-    for (let j = i + 1; j < i + 3; j++) {
-      arr2.push(nums[j]);
+  let answer = 0;
+  let arr = getCombinations(nums, 3);
+  arr.forEach((e) => {
+    let num = e.reduce((acc, cur) => acc + cur);
+    let result = 0;
+    for (let i = 1; i <= num; i++) {
+      if (num % i === 0) {
+        result += 1;
+      }
     }
-    arr.push(arr2);
-  }
-  console.log(arr);
+
+    result === 2 ? (answer += 1) : answer;
+  });
 
   return answer;
 }
-console.log(solution([1, 2, 3, 4]));
+console.log(solution([1, 2, 7, 6, 4]));
