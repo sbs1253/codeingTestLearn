9b1f8e3b379b142b1eb97ae0c331250f6b93d1ed
songbbang <rokaf340@gmail.com>
2024-01-22 15:12:31 +0900

    기사단원의 무기풀이

--- Lv1/기사단원의 무기.js
+++ Lv1/기사단원의 무기.js
@@ -1,5 +1,19 @@
 function solution(number, limit, power) {
-  var answer = 0;
+  let answer = 0;
+  let arr = [];
+  for (let i = 1; i <= number; i++) {
+    let num = 0;
+    for (let j = 1; j <= Math.sqrt(i); j++) {
+      if (i / j === j) {
+        num -= 1;
+      }
+      if (i % j === 0) {
+        num += 2;
+      }
+    }
+    arr.push(num);
+  }
+  answer = arr.map((e) => (e <= limit ? e : power)).reduce((acc, cur) => acc + cur, 0);
   return answer;
 }
-console.log(solution(5, 3, 2));
+console.log(solution(10, 3, 2));
