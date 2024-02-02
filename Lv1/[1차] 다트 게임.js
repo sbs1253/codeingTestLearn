8207b5a26b84dec0025709d005c0ce656c08bc4c
songbbang <rokaf340@gmail.com>
2024-02-02 15:35:47 +0900

    kakao 다트게임

--- Lv1/[1차] 다트 게임.js
+++ Lv1/[1차] 다트 게임.js
@@ -1,4 +1,25 @@
 function solution(dartResult) {
-  var answer = 0;
-  return answer;
+  let arr = [];
+  let str = dartResult.match(/\D+/g);
+  let num = dartResult.match(/\d+/g);
+  str.forEach((e, i) => {
+    [...e].forEach((v) => {
+      if (v == 'S') {
+        arr[i] = Math.pow(num[i], 1);
+      } else if (v == 'D') {
+        arr[i] = Math.pow(num[i], 2);
+      } else if (v == 'T') {
+        arr[i] = Math.pow(num[i], 3);
+      }
+      if (v == '*') {
+        arr[i - 1] *= 2;
+        arr[i] *= 2;
+      } else if (v == '#') {
+        arr[i] *= -1;
+      }
+    });
+  });
+  return arr.reduce((a, b) => a + b);
 }
+
+console.log(solution('1D2S#10S	'));
