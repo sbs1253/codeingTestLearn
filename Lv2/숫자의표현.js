f8d527e65e0d5056291815ca8a4e9e160c445688
songbbang <rokaf340@gmail.com>
2024-02-09 16:03:31 +0900

    숫자의 표현 문제풀이

--- Lv2/숫자의표현.js
+++ Lv2/숫자의표현.js
@@ -1,4 +1,15 @@
 function solution(n) {
-  var answer = 0;
+  let answer = 1;
+  for (let i = 1; i <= Math.floor(n / 2); i++) {
+    let result = i;
+    for (let j = i + 1; j <= Math.ceil(n / 2); j++) {
+      result += j;
+      if (result == n) {
+        answer += 1;
+        break;
+      } else if (result > n) break;
+    }
+  }
   return answer;
 }
+console.log(solution(15));
