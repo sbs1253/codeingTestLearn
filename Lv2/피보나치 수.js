5d2e8b9045165296166592691025940f9564377a
songbbang <rokaf340@gmail.com>
2024-02-09 16:45:05 +0900

    피보나치 수 문제풀이

---
+++ Lv2/피보나치 수.js
@@ -0,0 +1,13 @@
+function solution(n) {
+  let answer = 0;
+  let arr = Array(n + 1).fill(0);
+  arr[1] = 1;
+  arr.forEach((_, i) => {
+    if (i > 1) {
+      arr[i] = ((arr[i - 1] % 1234567) + (arr[i - 2] % 1234567)) % 1234567;
+    }
+  });
+  answer = arr[n];
+  return answer;
+}
+console.log(solution(1500));
