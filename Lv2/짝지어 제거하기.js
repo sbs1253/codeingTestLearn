a438f73c348c52851187b3ae7b008861402aa6bc
songbbang <rokaf340@gmail.com>
2024-02-09 17:31:58 +0900

    짝지어 제거하기 (스택풀이)

---
+++ Lv2/짝지어 제거하기.js
@@ -0,0 +1,10 @@
+function solution(s) {
+  let arr = [];
+  for (let i = 0; i < s.length; i++) {
+    if (arr.length === 0 || arr[arr.length - 1] !== s[i]) {
+      arr.push(s[i]);
+    } else arr.pop();
+  }
+  return arr.length ? 0 : 1;
+}
+console.log(solution('baabaaa'));
