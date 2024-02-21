5e5d8688cd10b9fe22c10e3b3fda50af8ee942c2
songbbang <rokaf340@gmail.com>
2024-02-21 17:52:54 +0900

    문제풀이중

---
+++ Lv2/괄호 회전하기.js
@@ -0,0 +1,8 @@
+function solution(s) {
+  let answer = -1;
+  for (let i = 0; i < s.length; i++) {}
+  s = s.slice(1) + s.slice(0, 1);
+  console.log(s);
+  return answer;
+}
+console.log(solution('[](){}'));
