ce7e4e63bc69dc7073fb004118f37bc65a6462a2
rokaf340 <rokaf340@gmail.com>
2024-03-07 21:29:50 +0900

    문제풀이중

---
+++ Lv1/의상.js
@@ -0,0 +1,12 @@
+function solution(clothes) {
+  let answer = 0;
+  let obj = {};
+  clothes.forEach((e) => {
+    if (obj[e[1]]) obj[e[1]] += 1;
+    else obj[e[1]] = 1;
+  });
+  Object.values(obj).reduce((a, b) => a + b, 0) != Object.values(obj).reduce((a, b) => a * b, 1)
+    ? (answer = Object.values(obj).reduce((a, b) => a + b, 0) + Object.values(obj).reduce((a, b) => a * b, 1))
+    : (answer = Object.values(obj).reduce((a, b) => a + b, 0));
+  return answer;
+}
