285e671197e41a8573a3ec5818f1166071337b2c
songbbang <rokaf340@gmail.com>
2024-03-27 16:12:07 +0900

    주차요금계산 = 객체, date함수사용

--- Lv2/주차 요금 계산.js
+++ Lv2/주차 요금 계산.js
@@ -1,13 +1,41 @@
 function solution(fees, records) {
   let answer = [];
+  let obj = {};
   const today = new Date();
-  today.setHours(0, 0, 0, 0);
-  console.log(today.getHours());
+  records.forEach((e) => {
+    const [time, number, inOut] = e.split(' ');
+    if (!obj[number]) obj[number] = [];
+    obj[number].push({ time });
+  });
+  // console.log(obj);
+  Object.keys(obj)
+    .sort((a, b) => a - b)
+    .forEach((record) => {
+      let differenceInMinutes = 0;
+      let number = obj[record];
+      if (number.length % 2 !== 0) number.push({ time: '23:59' });
+      console.log(number);
+      for (let i = 0; i < number.length; i += 2) {
+        if (i + 1 < number.length) {
+          const [inHour, inMinute] = number[i]['time'].split(':').map(Number);
+          const [outHour, outMinute] = number[i + 1]['time'].split(':').map(Number);
+          const inTime = new Date(today.getFullYear(), today.getMonth(), today.getDate(), inHour, inMinute);
+          const outTime = new Date(today.getFullYear(), today.getMonth(), today.getDate(), outHour, outMinute);
+          differenceInMinutes += (outTime - inTime) / (1000 * 60);
+        }
+      }
+      answer.push(differenceInMinutes);
+    });
+  answer = answer.map((e) => {
+    if (e >= fees[0]) {
+      return (Math.ceil((e - fees[0]) / fees[2]) / 10) * 10 * fees[3] + fees[1];
+    } else return fees[1];
+  });
   return answer;
 }
 console.log(
   solution(
-    [180, 5000, 10, 600],
+    [180, 5000, 10, 1000],
     [
       '05:34 5961 IN',
       '06:00 0000 IN',
@@ -21,24 +49,3 @@ console.log(
     ]
   )
 );
-function calculateTimeDifference(inTimeStr, outTimeStr) {
-  // 입력 문자열에서 시간과 분을 추출
-  const [inHour, inMinute] = inTimeStr.split(' ')[0].split(':').map(Number);
-  const [outHour, outMinute] = outTimeStr.split(' ')[0].split(':').map(Number);
-
-  // 오늘 날짜를 기준으로 Date 객체 생성
-  const today = new Date();
-  const inTime = new Date(today.getFullYear(), today.getMonth(), today.getDate(), inHour, inMinute);
-  const outTime = new Date(today.getFullYear(), today.getMonth(), today.getDate(), outHour, outMinute);
-  // 두 시간의 차이를 밀리초 단위로 계산하고, 분으로 변환
-  const differenceInMinutes = Math.abs(outTime - inTime) / (1000 * 60);
-
-  return differenceInMinutes;
-}
-
-// 사용 예시
-const inRecord = '05:34 5961 IN';
-const outRecord = '07:59 5961 OUT';
-const timeDifference = calculateTimeDifference(inRecord, outRecord);
-
-console.log(`두 시간의 차이는 ${timeDifference}분 입니다.`);
