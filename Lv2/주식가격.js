66b9627f76e2bf97c0a01e9df70f43832b2fc1ac
songbbang <rokaf340@gmail.com>
2024-03-29 14:50:28 +0900

    문제풀이 => stack과 인덱스

--- Lv2/주식가격.js
+++ Lv2/주식가격.js
@@ -4,18 +4,15 @@ function solution(prices) {
 
   for (let i = 0; i < prices.length; i++) {
     while (stack.length > 0 && prices[i] < prices[stack[stack.length - 1]]) {
-      let topIndex = stack.pop();
-      answer[topIndex] = i - topIndex;
+      let price = stack.pop();
+      answer[price] = i - price;
     }
     stack.push(i);
   }
-
-  // 스택에 남은 인덱스 처리
   while (stack.length > 0) {
-    let topIndex = stack.pop();
-    answer[topIndex] = prices.length - 1 - topIndex;
+    let price = stack.pop();
+    answer[price] = prices.length - 1 - price;
   }
-
   return answer;
 }
 
