ea3fb01c48fc96afdfbe4871c88f1e3d800e7787
songbbang <rokaf340@gmail.com>
2024-04-18 17:20:50 +0900

    문제풀이 => 블럭제거 및 채우기

--- Lv2/[1차] 프렌즈4블록.js
+++ Lv2/[1차] 프렌즈4블록.js
@@ -1,30 +1,57 @@
 function solution(m, n, board) {
   let answer = 0;
-  let boards = Array.from({ length: m }, () => Array(n).fill(false));
   let arr = board.map((e) => e.split(''));
 
-  const check = () => {
+  function del() {
+    let boards = Array.from({ length: m }, () => Array(n).fill(false));
+    let state = false;
     for (let i = 0; i < m - 1; i++) {
       for (let j = 0; j < n - 1; j++) {
         let current = arr[i][j];
-        if (current === arr[i][j + 1] && current === arr[i + 1][j] && current === arr[i + 1][j + 1]) {
-          console.log(current);
-          let del = true;
+        if (
+          current === arr[i][j + 1] &&
+          current === arr[i + 1][j] &&
+          current === arr[i + 1][j + 1] &&
+          current !== '0'
+        ) {
+          state = true;
           boards[i][j] = boards[i][j + 1] = boards[i + 1][j] = boards[i + 1][j + 1] = true;
         }
       }
     }
-  };
+    if (state) {
+      boards.forEach((e, i) => {
+        e.forEach((el, idx) => {
+          if (el) {
+            arr[i][idx] = '0';
+            answer += 1;
+          }
+        });
+      });
+    }
+    return state;
+  }
 
-  boards.forEach((e, i) => {
-    e.forEach((el, idx) => {
-      if (el) {
-        arr[i][idx] = '0';
+  function drop() {
+    for (let j = 0; j < n; j++) {
+      for (let i = m - 1; i >= 0; i--) {
+        if (arr[i][j] === '0') {
+          for (let k = i - 1; k >= 0; k--) {
+            if (arr[k][j] !== '0') {
+              [arr[i][j], arr[k][j]] = [arr[k][j], arr[i][j]];
+              break;
+            }
+          }
+        }
       }
-    });
-  });
+    }
+  }
 
-  console.log(arr);
+  while (del()) {
+    drop();
+  }
+
   return answer;
 }
+
 console.log(solution(4, 5, ['CCBDE', 'AAADE', 'AAABF', 'CCBBF']));
