feb320c68be4c40b6f6e13ce734e1ad3641084fb
bbang <rokaf340@gmail.com>
2024-05-14 15:50:25 +0900

    문제풀이

--- Lv2/삼각달팽이.js
+++ Lv2/삼각달팽이.js
@@ -1,35 +1,29 @@
 function solution(n) {
-  let answer = [];
   let arr = Array.from({ length: n }, (_, i) => Array(i + 1).fill(0));
-  let run = [0, 0];
-  let right = [0, 1];
-  let left = [0, -1];
-  let up = [1, 0];
-  let down = [-1, 0];
+  let x = 0;
+  let y = 0;
+  let run = [
+    [1, 0],
+    [0, 1],
+    [-1, -1],
+  ];
+  let count = n;
+  let runCount = 0;
   let num = 1;
-  for (let i = 0; i < 10; i++) {
-    if (run[0] < arr.length - 1 && arr[run[0]][run[1]] == 0) {
-      arr[run[0]][run[1]] = num;
-      run[0] += up[0];
-      run[1] += up[1];
+  while (count >= 1) {
+    let go = run.shift();
+    for (let i = 1; i < count; i++) {
+      arr[x][y] = num;
       num++;
-    } else if (run[0] == arr.length - 1 && arr[run[0]][run[1]] == 0) {
-      arr[run[0]][run[1]] = num;
-      if (arr[run[0]][run[1] + 1] == 0) {
-        run[0] += right[0];
-        run[1] += right[1];
-      }
-      num++;
-    } else {
-      run[0] += down[0] + left[0];
-      run[1] += down[1] + left[1];
-      // console.log(run);
-      arr[run[0]][run[1]] = num;
-      num++;
+      x += go[0];
+      y += go[1];
     }
-    console.log(arr);
+    count = n - runCount;
+    runCount++;
+    run.push(go);
   }
-  return answer;
+  arr[x][y] = num;
+  return arr.flat();
 }
-console.log(solution(4));
+console.log(solution(6));
 // [1,2,9,3,10,8,4,5,6,7]
